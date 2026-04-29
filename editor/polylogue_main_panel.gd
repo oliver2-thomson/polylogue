7c20e3c89ed1cfe9815bc4fcc7645dd4405c02a3
Ollie <oliver2.thomson@live.uwe.ac.uk>
2026-04-29 15:07:31 +0100

    Fixed being able to delete start node

--- editor/polylogue_main_panel.gd
+++ editor/polylogue_main_panel.gd
@@ -69,21 +69,6 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.keycode == KEY_S && event.is_command_or_control_pressed():
 			redraw()
-			
-	#if event is InputEventMouseButton:
-		#if event.button_index == MOUSE_BUTTON_MIDDLE:
-			#is_panning = event.pressed
-			#last_mouse_pos = event.position
-			#accept_event()
-			#
-		##if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			##_on_popup_request(get_local_mouse_position())
-#
-	#elif event is InputEventMouseMotion and is_panning:
-		#var delta: Vector2 = event.position - last_mouse_pos
-		#scroll_offset -= delta / zoom
-		#last_mouse_pos = event.position
-		#accept_event()
 
 func _on_connection_to_empty(from_node: StringName, from_port: int, release_position: Vector2) -> void:
 	# create popup listing all classes
@@ -143,6 +128,7 @@ func _on_node_deselected(node: Node) -> void:
 func _delete_single_node(_uid: int):
 	var node_string_name = ""
 	for node in nodes_dict.values():
+		if node is StartNode: return
 		if node is PolylogueGraphNode:
 			if node.conversation_node.uid == _uid:
 				node_string_name = node.name
