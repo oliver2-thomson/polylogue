47b0cdc0e1cf7ebe9c47bc69e43624d6fa8a2f65
Ollie Desktop 2025 <oliver2.thomson@live.uwe.ac.uk>
2026-04-30 15:21:57 +0100

    Added a script template for the reader

---
+++ templates/script_templates/Reader/reader_exported_fields.gd
@@ -0,0 +1,65 @@
+extends Reader
+
+
+@export var character_title_label: Label
+@export var dialogue_label: Label
+@export var options_box: VBoxContainer
+@export var start_button: Button
+@export var advance_button: Button
+
+
+func _ready() -> void:
+	start_button.pressed.connect(_on_start_button_pressed)
+	advance_button.pressed.connect(_on_advance_button_pressed)
+	started.connect(_on_started)
+	exited.connect(_on_exited)
+	node_ready.connect(_on_node_ready)
+	polylogue_signal_emitted.connect(_on_polylogue_signal_emitted)
+	branch_requested.connect(_on_branch_requested)
+
+func _on_start_button_pressed() -> void:
+	start()
+
+func _on_advance_button_pressed() -> void:
+	advance()
+
+func _on_started() -> void:
+	character_title_label.show()
+	dialogue_label.show()
+	advance_button.show()
+	start_button.hide()
+	options_box.hide()
+
+func _on_exited(reason: String) -> void:
+	start_button.show()
+	character_title_label.hide()
+	dialogue_label.hide()
+	advance_button.hide()
+	options_box.hide()
+
+func _on_node_ready(node: PolylogueNodeBase) -> void:
+	# Reset the UI
+	character_title_label.text = ""
+	dialogue_label.text = ""
+	advance_button.show()
+	for child in options_box.get_children():
+		child.queue_free()
+	
+	# Present Node
+	if node is SpeechNode:
+		character_title_label.text = node.character.name
+		if node is LineNode:
+			dialogue_label.text = node.get_line()
+			if node is OptionNode:
+				advance_button.hide()
+				for option_index in len(node.get_options()):
+					var option = node.get_options()[option_index]
+					var new_button = Button.new()
+					new_button.text = option
+					new_button.pressed.connect(advance.bind(option_index))
+
+func _on_polylogue_signal_emitted(event_name: String, payload: Variant) -> void:
+	print("Signal: {0} emitted with payload: {1}".format([event_name, payload]))
+
+func _on_branch_requested(condition: String, payload: Variant) -> void:
+	advance(false)
